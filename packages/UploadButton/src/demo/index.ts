ab4bc8a9743f11d5b1e2b6f7fbe2c6754c8b0bf8
liqiang2 <spacecode@qq.com>
2022-01-25 18:11:17 +0800

    docs: change

--- packages/UploadButton/src/demo/index.ts
+++ packages/UploadButton/src/demo/index.ts
@@ -7,7 +7,7 @@
 
 /**
  * @name 基本用法
- * @description 按钮自带四种尺寸,在加载效果时按钮禁用点击。特别需求可通过对组件style样式进行大小、颜色调整控制。
+ * @description 按钮自带四种尺寸,在加载效果时按钮禁用点击。颜色跟随Arco主题，特别需求可通过对组件style样式进行大小、颜色调整控制。
  */
 import Basic from './basic.vue';
 
