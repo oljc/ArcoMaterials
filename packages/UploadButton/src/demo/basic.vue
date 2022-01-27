7b5ce19cba55e109077ce8ff2dc82a846a0ee736
liqiang2 <spacecode@qq.com>
2022-01-27 21:27:57 +0800

    fix: unable to preview

--- packages/UploadButton/src/demo/basic.vue
+++ packages/UploadButton/src/demo/basic.vue
@@ -2,11 +2,11 @@
   <div class="wrap">
     <UploadButton style="margin: 0 10px" size="mini">mini</UploadButton>
     <UploadButton style="margin: 0 10px" size="small">small</UploadButton>
+    <UploadButton style="margin: 0 10px">提交</UploadButton>
+    <UploadButton style="margin: 0 10px" type="square">形状</UploadButton>
     <UploadButton style="margin: 0 10px" size="large" :success-duration="800"
       >这是一段较长的文字</UploadButton
     >
-    <UploadButton style="margin: 0 10px">提交</UploadButton>
-    <UploadButton style="margin: 0 10px" type="square">形状</UploadButton>
     <UploadButton style="margin: 0 10px" disabled>禁用</UploadButton>
   </div>
 </template>
