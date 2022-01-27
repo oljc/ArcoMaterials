9cf0a58049646d7083fb9c0f3bdf262cf8c7b36a
liqiang2 <spacecode@qq.com>
2022-01-27 21:14:20 +0800

    fix: unable to preview

--- packages/UploadButton/src/demo/basic.vue
+++ packages/UploadButton/src/demo/basic.vue
@@ -1,16 +1,19 @@
 <template>
-  <UploadButton style="margin: 0 10px" size="mini">mini</UploadButton>
-  <UploadButton style="margin: 0 10px" size="small">small</UploadButton>
-  <UploadButton style="margin: 0 10px" size="large" :success-duration="800"
-    >这是一段较长的文字</UploadButton
-  >
-  <UploadButton style="margin: 0 10px">提交</UploadButton>
-  <UploadButton style="margin: 0 10px" type="square">形状</UploadButton>
-  <UploadButton style="margin: 0 10px" disabled>禁用</UploadButton>
+  <div class="wrap">
+    <UploadButton style="margin: 0 10px" size="mini">mini</UploadButton>
+    <UploadButton style="margin: 0 10px" size="small">small</UploadButton>
+    <UploadButton style="margin: 0 10px" size="large" :success-duration="800"
+      >这是一段较长的文字</UploadButton
+    >
+    <UploadButton style="margin: 0 10px">提交</UploadButton>
+    <UploadButton style="margin: 0 10px" type="square">形状</UploadButton>
+    <UploadButton style="margin: 0 10px" disabled>禁用</UploadButton>
+  </div>
 </template>
 
 <script lang="ts">
-import UploadButton from 'arco-vue-UploadButton';
+// eslint-disable-next-line import/no-unresolved
+import UploadButton from 'arco-vue-upload-button';
 
 export default {
   name: 'BasicDemo',
@@ -19,3 +22,12 @@ export default {
   },
 };
 </script>
+<style>
+.wrap {
+  width: 100%;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
